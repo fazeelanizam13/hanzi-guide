8a81daf43cdbead8b09da5389a58c38d244e215a
Fazeela Nizam <fazeelanizam13@gmail.com>
2020-12-12 23:46:54 +0530

    New build files

---
+++ precache-manifest.10a6000a82ee8e844d6b2c4beb526a26.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34242755e0a03f8736c178b42648e5d5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee7a8eba28f0ea303e32",
+    "url": "./static/css/main.1d245379.chunk.css"
+  },
+  {
+    "revision": "7ef68586552e498f6c69",
+    "url": "./static/js/2.8d7f1eeb.chunk.js"
+  },
+  {
+    "revision": "9fda8197ca6e25979aa843f42451bfaf",
+    "url": "./static/js/2.8d7f1eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee7a8eba28f0ea303e32",
+    "url": "./static/js/main.87b44681.chunk.js"
+  },
+  {
+    "revision": "b029cd185833a27082e4",
+    "url": "./static/js/runtime-main.ff99e905.js"
+  },
+  {
+    "revision": "b697b5faeba63e4d32b4942719897ae7",
+    "url": "./static/media/CormorantGaramond-Bold.b697b5fa.ttf"
+  },
+  {
+    "revision": "3ccaf14c005c43d53ad96aaf7409489b",
+    "url": "./static/media/CormorantGaramond-SemiBold.3ccaf14c.ttf"
+  },
+  {
+    "revision": "0b3d1b5cb0a45a85cf00b1b4c53067bf",
+    "url": "./static/media/NotoSerifSC-SemiBold.0b3d1b5c.otf"
+  },
+  {
+    "revision": "962021905baaca70087ced77a5475d3b",
+    "url": "./static/media/lantern.96202190.svg"
+  },
+  {
+    "revision": "4965a8cfc489a6ed836cd6d4076275ed",
+    "url": "./static/media/posters-chinese-lantern-hang-on-cherry-tree.4965a8cf.jpg"
+  }
+]);
